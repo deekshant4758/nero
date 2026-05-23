b0b4f972698959bd9b9732c8093df55d64e8190b
Deekshant Gupta <deekshant2003@gmail.com>
2026-05-24 01:52:09 +0530

    fix: removed workflow and server app instead of static

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,17 +2,15 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
+  output: "standalone",
+
   ...(basePath
     ? {
         basePath,
         assetPrefix: basePath,
       }
     : {}),
-}
+};
 
-export default nextConfig
+export default nextConfig;
